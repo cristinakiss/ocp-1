6d02b7b35c9a55e535995d8727351d28cb0592e1
Cristina Kiss <Cristina.Kiss@endava.com>
2020-05-12 17:35:23 +0300

    [CH-1] jar file tool

---
+++ src/main/java/eu/cristinakiss/cert/ocp1/ch1/jartest/Sunny.java
@@ -0,0 +1,8 @@
+package eu.cristinakiss.cert.ocp1.ch1.jartest;
+
+public class Sunny {
+
+    public static void main(String[] args) {
+        System.out.println("today it will be sunny");
+    }
+}
---
+++ src/main/java/eu/cristinakiss/cert/ocp1/ch1/jartest2/Rainy.java
@@ -0,0 +1,8 @@
+package eu.cristinakiss.cert.ocp1.ch1.jartest2;
+
+public class Rainy {
+
+    public static void main(String[] args) {
+        System.out.println("today was supposed to be rainy, but it's sunny instead!");
+    }
+}
