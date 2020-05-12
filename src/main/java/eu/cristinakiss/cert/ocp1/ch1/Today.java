cee738006fcf82b949b7c2af46f2f4f0d193fc3e
Cristina Kiss <Cristina.Kiss@endava.com>
2020-05-12 17:26:52 +0300

    [CH-1] Imports more

---
+++ src/main/java/eu/cristinakiss/cert/ocp1/ch1/Today.java
@@ -0,0 +1,10 @@
+package eu.cristinakiss.cert.ocp1.ch1;
+
+import java.time.LocalDate;
+
+public class Today {
+
+    public static void main(String[] args) {
+        System.out.println("Today is : " + LocalDate.now());
+    }
+}
